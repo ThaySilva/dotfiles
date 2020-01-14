08481f164ff3ef0f4cc11adc1ce7bf1d8f07a947
Thaynara Silva <silvathaynara21@gmail.com>
2020-01-14 11:27:30 +0000

    Update daily_update_ubuntu.sh

--- bash/daily_update_ubuntu.sh
+++ bash/daily_update_ubuntu.sh
@@ -11,6 +11,6 @@ echo "Apt update executed on $DATE"
 ## Read current file count
 cd $HOME
 result=`du -chd 0`
-total="${result%%G*}G"
+total="${result%%G*}"
 
 echo "The current file count in your home directory is $total"
